2cb21e041bfc08f6a929b36bcb9ded9eca8478c0
Luis Fernando <fernu292@gmail.com>
2022-01-08 16:42:14 -0600

    Finalizando reto assert

--- assert_staments.py
+++ assert_staments.py
@@ -7,6 +7,7 @@ def run():
     num = input("Ingrese un numero: ")
     
     #Assert funciona con una condicion, mensaje de error
+    assert int(num) > 0,"Ingresa valores positivos" 
     assert num.isnumeric(), "Debes ingresar datos numericos" # Retorna si un string es tipo numerico
     print(divisors(int(num)))
     print("Termino mi programa")
