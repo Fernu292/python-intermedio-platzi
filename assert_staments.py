78d97ac83e91b53283c8a73e7d46ff3ffe7d4c19
Luis Fernando <fernu292@gmail.com>
2022-01-08 16:38:55 -0600

    Agregando errores

---
+++ assert_staments.py
@@ -0,0 +1,16 @@
+def divisors(num):
+    divisor = [i for i in range(1,num+1) if num%i==0]
+    return divisor
+
+def run():
+    
+    num = input("Ingrese un numero: ")
+    
+    #Assert funciona con una condicion, mensaje de error
+    assert num.isnumeric(), "Debes ingresar datos numericos" # Retorna si un string es tipo numerico
+    print(divisors(int(num)))
+    print("Termino mi programa")
+    
+        
+if __name__ == "__main__":
+    run()
--- debugging.py
+++ debugging.py
@@ -1,11 +1,18 @@
 def divisors(num):
+    if num < 0:
+        raise ValueError("Ingresa un valor positivo")
     divisor = [i for i in range(1,num+1) if num%i==0]
     return divisor
 
 def run():
-    num = int(input("Ingrese un numero: "))
-    print(divisors(num))
-    print("Termino mi programa")
-
+    try: 
+        num = int(input("Ingrese un numero: "))
+        print(divisors(num))
+        print("Termino mi programa")
+    #Atrapar errores difiniendolo como ve
+    except ValueError as ve: 
+        print(ve)
+        run()
+        
 if __name__ == "__main__":
     run()
